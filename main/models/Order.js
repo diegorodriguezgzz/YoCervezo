adadbc91ade5b9d855c3094d00d00239dbd07e92
Santiago Hinojosa <santiago@Santiagos-MacBook-Pro.local>
2019-02-07 21:14:17 -0600

    modelo de user para onborading

--- main/models/Order.js
+++ main/models/Order.js
@@ -3,6 +3,34 @@ const Schema = mongoose.Schema;
 
 const orderSchema = new Schema({
 
+    malt2: String,
+    malt1: String,
+    malt3: String,
+    malt4: String, //rara vez hay cervezas de + de 4
+    malt5: String,
+    qtyMalt1: Number,
+    qtyMalt2: Number,
+    qtyMalt3: Number,
+    qtyMalt4: Number,
+    qtyMalt5: Number,
+    hop1: String,
+    hop2: String,
+    hop3: String,
+    hop4: String,
+    hop5: String,
+    hop6: String,
+    qtyHop1: String,
+    qtyHop2: String,
+    qtyHop3: String,
+    qtyHop4: String,
+    qtyHop5: String,
+    qtyHop6: String,
+    dryHopped: Boolean,
+    dhHop1: String,
+    dhHop2: String,
+    qtyDHop1: Number,
+    qtyDHop2: Number,
+
 });
 
 const Order = mongoose.model('Order', orderSchema);
--- main/models/User.js
+++ main/models/User.js
@@ -2,7 +2,28 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-
+    username: String,
+    password: String,
+    country: String,
+    province: String,
+    street: String,
+    number: Number,
+    email: String,
+    zipCode: Number,
+    bestBeer: String, //Rel. a Beer Model? 
+    worstBeer: String,  //Rel. a Beer Model? 
+    celebBeer: String, //Cerveza que usas para celebrar
+    sadBeer: String, // Cerveza cuando estas triste/de malas
+    beerColour: String, //De que color prefieres tu cerveza? 
+    beerHead: String, //Cuán espumosa te gusta tu cerveza? 
+    beerGas: String, //La cerveza te gusta con mucho/medio/poco/nada de gas
+    beerAlc: String, //Te gustan las cervezas con Mucho/algo/poco/nada de alcohol
+    beerBitt: String, //Te gustan las cervezas muy amargas/ medio amargas / nada amargas /
+    beerArray: [String], //Rel. con el beer database Array con las cervezas que has probado
+    beerLikeArray: [String], //Rel. con el beer database Array con las 3 cervezas que más te han gustado
+    beerDislikeArraY: [String], //Rel. con el beer database Array con las 3 cervezas que menos te gustan
+    beerInterest: [String], //Bebedor de sofá, Entusiasta, Entrepreneur 
+    belt: Number //Nivel del cervezador dentro del 
 });
 
 const User = mongoose.model('User', userSchema);
