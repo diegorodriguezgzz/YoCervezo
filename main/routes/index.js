b460af3626ee4a33cdc4d977b37c323457f59a32
Santiago Hinojosa <santiago@Santiagos-MacBook-Pro.local>
2019-02-11 10:35:13 -0600

    testing upstream merge

--- main/routes/index.js
+++ main/routes/index.js
@@ -7,6 +7,11 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+router.get('/onboarding', (req,res,next)=>
+{
+res.render('onboarding')
+});
+
 router.get('/beers', (req, res, next) => {
   Beer.find()
   .then(beers => {
@@ -27,5 +32,16 @@ router.get('/beers/:id', (req, res, next) => {
     next();
   });
 });
+
+//router.post('/newUser', (req,res)=>{
+//  const{username, password, country, province, street, number, email, zipCode, bestBeer, worstBeer, celebBeer, beerStyle, beerHead, beerGas, beerAlc, beerBitt}
+//  const newUser = new User ({username, password, country, province, street, number, email, zipCode, bestBeer, worstBeer, celebBeer, beerStyle, beerHead, beerGas, beerAlc, beerBitt})
+//  newUser.save()
+//  .then ((user))=>{
+//   res.redirect('/beers')
+//  })
+//  .catch(err=> console.log(err))
+//})
+
 
 module.exports = router;
