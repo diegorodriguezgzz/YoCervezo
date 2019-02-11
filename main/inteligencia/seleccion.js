c8b1bc136ac7204c061912bf54855fc76e78ae45
Diego Rodríguez <diegorodriguezgzz@gmail.com>
2019-02-11 00:00:09 -0600

    Multiseleccion cervezas

---
+++ main/inteligencia/seleccion.js
@@ -0,0 +1,11 @@
+/* 
+4 casos:
+A) Rango amplio de cervezas probadas, amargor ideal identificado
+B) Rango angosto de cervezas probadas, amargor bajo preferido
+C) Rango angosto de cervezas probadas, amargor alto preferido
+  => Sugerir algo ligeramente más amargo, no demasiado
+
+Lo mismo aplica para ABV
+
+Tratar de variar los estilos
+*/
